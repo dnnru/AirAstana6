abd3cd24659dacb999fc9bc6f9c0b5e0c545f1f2
George Saveliev <George_Saveliev@hotmail.com>
2021-12-24 09:13:15 +0300

    Add project files.

---
+++ Components/Models/PortalMenuLinks.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AirAstana.Themes.AirAstana6.Components.Models
+{
+    public class PortalMenuLinks
+    {
+        public int PortalId { get; set; } = -1;
+        public string PortalName { get; set; } = "";
+        public List<MenuLink> MenuLinks { get; set; } = new List<MenuLink>();
+    }
+}
